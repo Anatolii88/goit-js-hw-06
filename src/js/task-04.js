aa01d685f3cce50b5dad1071124406ce9bd531fc
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-03-15 01:42:57 +0200

    Corrects 4 task

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,21 +1,23 @@
 let counterValue = 0;
 
 const decrementEl = document.querySelector('[data-action="decrement"]')
-console.log(decrementEl)
 
 const incrementEl = document.querySelector('[data-action="increment"]')
-console.log(incrementEl)
 
 const valueEl = document.querySelector('#value')
-console.log(valueEl)
 
-decrementEl.addEventListener('click', () => { 
-  counterValue -= 1
-  valueEl.textContent = counterValue
-  console.log(counterValue)
-})
-incrementEl.addEventListener('click', () => { 
+
+
+decrementEl.addEventListener('click', decrease)
+incrementEl.addEventListener('click',  increase)
+
+
+function increase() { 
   counterValue += 1
   valueEl.textContent = counterValue
-  console.log(counterValue)
-})
+}
+
+function decrease() { 
+  counterValue += 1
+  valueEl.textContent = counterValue
+}
