d0dd78aa70598154d18b8f1c39371b221afe8e1d
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-03-14 23:23:45 +0200

    Add second task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients')
+
+
+function addElement(array) { 
+
+  const newArray = []
+
+  array.forEach(function (element) { 
+    const itemEl = document.createElement('li')
+    itemEl.textContent = element
+    itemEl.classList.add('item')
+    newArray.push(itemEl)
+  })
+  
+  return ingredientsEl.append(...newArray)
+  
+}
+addElement(ingredients)
