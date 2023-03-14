d37df83a88c034913cc9e54a0532005fe123c93e
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-03-14 23:34:07 +0200

    Second solution way task number of two

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -20,8 +20,25 @@ function addElement(array) {
     itemEl.classList.add('item')
     newArray.push(itemEl)
   })
-  
+
   return ingredientsEl.append(...newArray)
   
 }
 addElement(ingredients)
+
+
+// - - - - - - - - Второй способ решения задания   - - - - - - - -
+
+
+// function addElement(array) { 
+
+//   return array.map(function (element) { 
+//     const itemEl = document.createElement('li')
+//     itemEl.textContent = element
+//     itemEl.classList.add('item')
+//     return itemEl
+//   })
+  
+// }
+// const newArray = addElement(ingredients)
+// ingredientsEl.append(...newArray)
