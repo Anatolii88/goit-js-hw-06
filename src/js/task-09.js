54711085502469117d585b10fba71889c4cc49f8
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-03-16 22:41:26 +0200

    Add 9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const colorEl = document.querySelector('.color')
+
+const buttonEl = document.querySelector('.change-color')
+buttonEl.addEventListener('click', onBtn)
+
+function onBtn() { 
+  document.body.style.backgroundColor = getRandomHexColor()
+  colorEl.textContent = document.body.style.backgroundColor
+}
+
