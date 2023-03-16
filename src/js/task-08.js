cdf44dff79d25d1de30e0199ff860762c8ed143f
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-03-16 02:23:16 +0200

    Add 8 task without method reset

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form')
+const user = {};
+
+form.addEventListener('submit', onSubmitForm)
+
+
+function onSubmitForm(event) { 
+  event.preventDefault()
+
+  const mail = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (!mail || !password) {
+    alert('Все поля должны быть заполнены!')
+  } else { 
+    user.email = mail
+    user.password = password
+    console.log(user)
+  }
+
+}
+
 
